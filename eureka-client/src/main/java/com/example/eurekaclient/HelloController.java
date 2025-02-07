b5d62b63d4530174dfd6d002abd61a120094a7bc
HappyYan <1483074850@qq.com>
2025-02-07 15:56:36 +0800

    Create HelloController.java

---
+++ eureka-client/src/main/java/com/example/eurekaclient/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.eurekaclient;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello from Eureka Client!";
+    }
+}
